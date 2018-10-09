29c30411e27de1eaf25a0a4d39906898a9db14b8
DjangoJuice <dungeonmesher@gmail.com>
2018-10-08 23:24:44 -0400

    Adding Handlebars

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.set("view engine", "handlebars");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
+app.get("/", function(req, res) {
+    res.render("index");
+})
+
 
 
 // Start the server
