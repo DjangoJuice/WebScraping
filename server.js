e58becfaf5a9d08b7990976ff987c57ca7f2f098
DjangoJuice <dungeonmesher@gmail.com>
2018-10-08 23:08:19 -0400

    Configuring Basic Server

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
