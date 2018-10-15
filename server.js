6de944709de0034746a60f95ce382b78777a2d61
DungeonMesher <32795703+DungeonMesher@users.noreply.github.com>
2018-10-14 21:55:20 -0400

    It's not web scraping

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // models for Mongo DB
-// var db = require("./models");
+var db = require("./models/Articles");
+
 
 // Web Ports
 var PORT = process.env.PORT || 3000;
@@ -32,6 +33,34 @@ mongoose.connect(MONGODB_URI);
 app.get("/", function(req, res) {
     res.render("index");
 })
+
+app.get("/scrape", function(req, res) {
+    axios.get("https://www.reuters.com/finance/markets/us").then(function(res) {
+    var $ = cheerio.load(res.data);
+    //console.log("res.data", res.data.children("a").children("h3").text())
+    // Store $crape results
+    var result = {};
+
+    // Add the text and href of every link, and save them as properties of the result object
+    //.children("a").children("h3").text()
+    result.title = $(this).children();
+    result.link = $(this).children("a").attr("href");
+
+     console.log("testing results", result)
+
+     // $crape results go into the DB for displaying to the DOM later
+     db.Article.create(result)
+     .then(function(dbArticle) {
+       // View the added result in the console
+       console.log("Trying`````````````", dbArticle);
+     })
+     .catch(function(err) {
+       // If an error occurred, send it to the client
+       return res.json(err);
+     });
+ });
+res.send("Complete")
+});
 
 
 
